017a8cb1e99b81aa8e8510df43e4a335a3793c17
Sengege <trace_tristan@126.com>
2018-03-11 23:53:25 +0800

    first commit for ci

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: python2.7
+@author: ‘trace_tristan@126.com‘
+@file: main
+@time: 11/3/18 下午11:51
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello CI!'
